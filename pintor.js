f0fb2eacb8b0dffe9ff9b9835f42d676cb56c40d
castelomabil11 <65201445+castelomabil11@users.noreply.github.com>
2020-06-03 14:52:37 -0600

    Create pintor.js

---
+++ pintor.js
@@ -0,0 +1,8 @@
+const express = require('express');
+let pintor = require('../controllers/pintoresController.js');
+
+let router = express.Router();
+
+router.get('/', pintor.list);
+
+module.exports = router;
